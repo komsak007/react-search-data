d6ecca874abe27dee26ea23d785deef935349cd3
komsak007 <lert_zodiac@hotmail.com>
2021-08-31 13:33:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import {
+  Input,
+  Table,
+  Container,
+  Form,
+  Header,
+  Button,
+} from "semantic-ui-react";
+
+import dataJson from "./data.json";
 
 function App() {
+  const [name, setName] = useState("");
+  const [filterData, setFilterData] = useState([]);
+
+  const filterHandler = async (e) => {
+    e.preventDefault();
+    const filter = dataJson.slice(1).filter((i) => i.C.includes(name));
+
+    setFilterData(filter);
+    // console.log(dataJson.slice(1, 2));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="container">
+      <Header as="h2">
+        <br />
+        <br />
+        React Google Sheet
+      </Header>
+
+      <Form onSubmit={filterHandler}>
+        <Input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          style={{ width: "100%" }}
+        />
+        <Button type="submit" style={{ marginTop: "5px" }}>
+          Submit
+        </Button>
+      </Form>
+
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Age</Table.HeaderCell>
+            <Table.HeaderCell>Salary</Table.HeaderCell>
+            <Table.HeaderCell>Hobby</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {filterData.map((item, index) => (
+            <Table.Row key={index}>
+              <Table.Cell>{item.A}</Table.Cell>
+              <Table.Cell>{item.B}</Table.Cell>
+              <Table.Cell>{item.C}</Table.Cell>
+              <Table.Cell>{item.D}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </Container>
   );
 }
 
